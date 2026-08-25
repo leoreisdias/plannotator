be8d23a363c598d26089bb38afbf5a3c3bb15ce3
Leonardo R. Dias <47978193+leoreisdias@users.noreply.github.com>
2026-08-24 23:44:46 -0300

    Regenerate canary viewer manifest

--- packages/core/guide-viewer-manifest.ts
+++ packages/core/guide-viewer-manifest.ts
@@ -5,10 +5,10 @@
 import type { GuideViewerAssets } from "./guide-format";
 
 export const GUIDE_VIEWER_MANIFEST: Omit<GuideViewerAssets, "baseUrl"> = {
-  js: "viewer.uL52DYtG.js",
-  css: "viewer.2q74Zm1i.css",
-  jsIntegrity: "sha384-uRtNq4DUwcGj8XipC2+e1WVTjrtIWMVVL9X3rlnSZQjbOP1mw9SC8GV67sH3sCYC",
-  cssIntegrity: "sha384-kpEQJ6JOE4oDHBIoRTDPTS2D0XmrqGry4zR4n+wnQKVl6dpD6t7bDGNz8bKuLW3i",
+  js: "viewer.Cax23-oo.js",
+  css: "viewer.DZjY1kcP.css",
+  jsIntegrity: "sha384-4XulB45wYWqEBwxBfvG6XfFy5mBTdF1RxZiQll4SqXzWy7mUR9kJhXkHQscIxvHv",
+  cssIntegrity: "sha384-RTAZGxhqu3V8M4qnhSa/EYBX2uyuZbM0RWhV2gvMnWN0aK2D+nFqiFKlkehYSzVw",
   langs: {
     "astro": "chunks/astro.BykyiR6i.js",
     "c": "chunks/c.BIGW1oBm.js",
